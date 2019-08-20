1125a3368e3018ab5d61852655747b722aeeacf2
weiqin <weiqin199711@163.com>
2019-08-20 19:32:22 +0800

    add react build file

---
+++ public/precache-manifest.b05988fbaf9a985fec089c9ed182a8eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba50077f1d4ad3ad8ce3f201d6eed256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "285cb5bcef080bf77adf",
+    "url": "/static/css/2.9ad894dd.chunk.css"
+  },
+  {
+    "revision": "9ea6b519f2e1184ffb3b",
+    "url": "/static/css/main.9961fe37.chunk.css"
+  },
+  {
+    "revision": "285cb5bcef080bf77adf",
+    "url": "/static/js/2.c98c0573.chunk.js"
+  },
+  {
+    "revision": "9ea6b519f2e1184ffb3b",
+    "url": "/static/js/main.a96c3cc1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
